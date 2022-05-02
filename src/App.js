4876f7cc8e20effec73ea9587a63109791f32c16
wholespace <wholespace.214@gmail.com>
2022-05-03 00:00:35 +0900

    fix arcsData errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Globe from "react-globe.gl";
+import { useState, useEffect } from "react";
 
 function App() {
-  const N = 20;
-  const arcsData = [...Array(N).keys()].map(() => ({
-    startLat: (Math.random() - 0.5) * 180,
-    startLng: (Math.random() - 0.5) * 360,
-    endLat: (Math.random() - 0.5) * 180,
-    endLng: (Math.random() - 0.5) * 360,
-    color: [['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)], ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]]
-  }));
+
+  const [arcsData, setArcsData] = useState([]);
+
+  useEffect(() => {
+    let temp = [...Array(20).keys()].map(() => ({
+      startLat: (Math.random() - 0.5) * 180,
+      startLng: (Math.random() - 0.5) * 360,
+      endLat: (Math.random() - 0.5) * 180,
+      endLng: (Math.random() - 0.5) * 360,
+      color: [['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)], ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]]
+    }));
+    setArcsData(temp);
+  }, [])
   return (
     <Globe
     globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
